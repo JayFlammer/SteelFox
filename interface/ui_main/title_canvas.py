33da4b9981c3b4a2a7c5a55ed3fb68cea84082d0
JayFlammer <156908335+JayFlammer@users.noreply.github.com>
2024-12-04 18:32:10 +0100

    ui changes

--- interface/ui_main/title_canvas.py
+++ interface/ui_main/title_canvas.py
@@ -2,6 +2,6 @@ import customtkinter as ctk
 
 def create_title_canvas(main_frame):
     """Erstellt den Titelstreifen"""
-    title_canvas = ctk.CTkCanvas(main_frame, bg="#2a3138", height=100, highlightthickness=0)
+    title_canvas = ctk.CTkCanvas(main_frame, bg="#C9B1BD", height=100, highlightthickness=0)
     title_canvas.pack(side="top", fill="x")
     return title_canvas
