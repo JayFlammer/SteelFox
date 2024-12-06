611780a3d5a38e9548c1637975ba6e22047740b0
JayFlammer <156908335+JayFlammer@users.noreply.github.com>
2024-12-06 09:24:56 +0100

    delete frame

--- interface/ui_main/frame_input.py
+++
@@ -1,7 +0,0 @@
-import customtkinter as ctk
-
-def create_input_frame(main_frame):
-    input_frame = ctk.CTkFrame(main_frame, fg_color="#7C6354", width=250, corner_radius=0)
-    input_frame.pack(side="left", fill="y")
-    input_frame.pack_propagate(False)
-    return input_frame
